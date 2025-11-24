a98bbf8c788aae1215bacfb3a608159c194aa1ca
Kirill Gmirya <Gmiryk_Kirill@mail.ru>
2025-11-24 19:05:54 +0300

    Ass base def for bot

--- ENG_words_teach_bot_code/main.py
+++ ENG_words_teach_bot_code/main.py
@@ -2,6 +2,30 @@ print("Hello! I am an English words teach bot!")
 
 from dotenv import load_dotenv
 import os
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+
 
 load_dotenv()
-secret = os.getenv("TG_BOT_TOKEN")
+TOKEN = os.getenv("TG_BOT_TOKEN")
+# TOKEN = input("Insert your TG token:")
+
+bot = AsyncTeleBot("TOKEN")
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=["help", "start"])
+async def send_welcome(message):
+    text = "Hi, I am EchoBot.\nJust write me something and I will repeat it!"
+    await bot.reply_to(message, text)
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+
+if __name__ == "__main__":
+    print("Bot is running")
+    asyncio.run(bot.polling())
